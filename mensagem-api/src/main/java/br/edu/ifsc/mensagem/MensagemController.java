d57e76394c86fb785f3395d08f8fef5734b8e634
mpisching <mpisching@gmail.com>
2020-08-13 21:50:00 -0300

    Projeto - Mensagem de Boas Vindas com Spring Boot na IDE Spring Tool Suite

--- mensagem-api/src/main/java/br/edu/ifsc/mensagem/MensagemController.java
+++ mensagem-api/src/main/java/br/edu/ifsc/mensagem/MensagemController.java
@@ -1,7 +1,6 @@
-package br.edu.ifsc.segundoexemplo;
+package br.edu.ifsc.mensagem;
 
 import java.util.concurrent.atomic.AtomicLong;
-
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MensagemController {
+	
 	private static final String template = "Olá, %s!";
 	
-	private final AtomicLong contador = new AtomicLong();
+	private final AtomicLong contador = new AtomicLong(); 
 	
 	@RequestMapping("/mensagem")
-	public Mensagem mensagem(@RequestParam(value="nome", defaultValue="Mundo") String
-			nome) {
+	public Mensagem mensagem(@RequestParam(value="nome", defaultValue="Mundo")
+			String nome) {
 		return new Mensagem(contador.incrementAndGet(), String.format(template, nome));
 	}
+	
 }
